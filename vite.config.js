42b98d9c97f724f10bea69bf66be85a0d5781d3b
LeafYeeXYZ <leafyeexyz@outlook.com>
2024-04-25 21:44:46 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    target: ['chrome108', 'edge108', 'firefox101', 'safari15.4'],
     rollupOptions: {
       output: {
         manualChunks: {
