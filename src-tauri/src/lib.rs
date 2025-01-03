6bd51640ab3212701b8cf11f226ae6fae5f286aa
LeafYeeXYZ <leafyeexyz@outlook.com>
2025-01-03 11:13:33 +0800

    feat: 添加单进程锁

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,12 @@
 use std::fs;
+use tauri::{AppHandle, Manager};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = show_window(app);
+        }))
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .invoke_handler(tauri::generate_handler![save_memory])
@@ -26,4 +30,15 @@ fn save_memory(path: &str, data: String) -> Result<String, String> {
         Ok(_) => Ok(path.to_string()),
         Err(e) => Err(e.to_string()),
     }
+}
+
+fn show_window(app: &AppHandle) {
+    let windows = app.webview_windows();
+
+    windows
+        .values()
+        .next()
+        .expect("Sorry, no window found")
+        .set_focus()
+        .expect("Can't Bring Window to Focus");
 }
